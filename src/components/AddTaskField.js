33bc9e43963b419d80fbd3f51e57e3d073fb1e54
fannywho <fanny.pourtier@hetic.net>
2017-11-22 16:17:32 +0100

    add liste des tâches + input pour en ajouter

---
+++ src/components/AddTaskField.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  color: grey;
+  margin: 5px;
+  font-weight: bold;
+`
+
+const Button = styled.button`
+  background-color: light-blue;
+  margin: 10px;
+  color: orange;
+`
+
+export default class AddTaskField extends Component {
+
+  render() {
+    const {
+      label,
+      onClick,
+    } = this.props
+
+    return(
+      <div>
+        <Label>{label}</Label>
+        <input type="text"/>
+        <Button onClick={onClick}>Ajoutay !</Button>
+      </div>
+    );
+  }
+}
