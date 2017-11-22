825ecc622990c9b232a2b40c1d90faa8dac436a4
Fanny Pourtier <fanny.pourtier@hetic.net>
2017-11-22 17:48:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return  null
-  }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+     )
+   }
 }
 
 export default App;
